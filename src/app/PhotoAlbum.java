041242937fd4b826f522738476ab8684f56ad992
vinceix <vincentxie12@gmail.com>
2016-03-27 00:20:19 -0400

    Added login page

--- src/app/PhotoAlbum.java
+++ src/app/PhotoAlbum.java
@@ -1,8 +1,26 @@
 package app;
 
-public class PhotoAlbum {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
 
+public class PhotoAlbum extends Application {
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		FXMLLoader loader = new FXMLLoader();   
+	    loader.setLocation(getClass().getResource("/view/Login.fxml"));
+	    AnchorPane root = (AnchorPane)loader.load();
+	    
+	    Scene scene = new Scene(root, 800, 600);
+	    primaryStage.setScene(scene);
+	    primaryStage.setResizable(false);
+	    primaryStage.show(); 
+	}
+	
 	public static void main(String[] args) {
-		System.out.println("This gon be photo album");
+		launch(args);
 	}
 }
