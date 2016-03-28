bc7207b7da596a1783f45b590db0b2e6cdca4d18
vinceix <vincentxie12@gmail.com>
2016-03-28 16:01:58 -0400

    Login user implemented. Improvements to login page.

--- src/controller/LoginController.java
+++ src/controller/LoginController.java
@@ -1,15 +1,18 @@
 package controller;
 
 import java.io.IOException;
+
+import app.PhotoAlbum;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.control.Alert.AlertType;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import model.NonAdminUser;
 
 /**
  * @author Edmond Wu & Vincent Xie
@@ -25,8 +28,15 @@ public class LoginController extends Controller {
 	@FXML
 	private Button Login;
 	
+	@FXML
+	private Text userwrong;
+	
+	@FXML
+	private Text passwrong;
+	
 	public void start(Stage mainStage) {                
 		setController(mainStage);
+	    Login.setDefaultButton(true);
 	}
 	
 	/**
@@ -38,43 +48,58 @@ public class LoginController extends Controller {
 		String username = Username.getText();
 		String password = Password.getText();
 		if(username.equalsIgnoreCase("admin")){
+			FXMLLoader loader = new FXMLLoader();   
+			loader.setLocation(getClass().getResource("/view/Admin.fxml"));
+		    AnchorPane root = (AnchorPane)loader.load();
+		    AdminController Admin = loader.getController();
+		    Admin.start(stage);
+		    
+		    Scene scene = new Scene(root, 800, 600);
+		    stage.setScene(scene);
+		    stage.setResizable(false);
+		    stage.show(); 
 			return;
 		} 
 		//search through list
-		
-		//if found
-		FXMLLoader loader = new FXMLLoader();   
-		loader.setLocation(getClass().getResource("/view/Admin.fxml"));
-	    AnchorPane root = (AnchorPane)loader.load();
-	    AdminController Admin = loader.getController();
-	    Admin.start(stage);
-	    
-	    Scene scene = new Scene(root, 800, 600);
-	    stage.setScene(scene);
-	    stage.setResizable(false);
-	    stage.show(); 
-		
-		/* Alert error = new Alert(AlertType.INFORMATION);
-		error.setHeaderText("Invalid Login!");
-		error.setContentText("Incorrect username and/or password.");
-		error.show();
+		for(int i = 0; i < PhotoAlbum.userArrayList.size(); i++){
+			NonAdminUser user = PhotoAlbum.userArrayList.get(i);
+			if(user.getUsername().equals(username)){
+				if(user.getPassword().equals(password)){
+					Username.setStyle("-fx-text-box-border: white; -fx-focus-color: #008ED6;");
+					userwrong.setStyle("-fx-opacity: 0;");
+					Password.setStyle("-fx-text-box-border: white; -fx-focus-color: #008ED6;");
+					passwrong.setStyle("-fx-opacity: 0;");
+					System.out.println("Login successful.");
+					return;
+				} else {
+					passwrong.setStyle("-fx-opacity: 1;");
+					Password.setStyle("-fx-text-box-border: red; -fx-focus-color: red;");
+					return;
+				}
+			}
+		}
+		//if not found
+		userwrong.setStyle("-fx-opacity: 1;");
 		Username.setStyle("-fx-text-box-border: red; -fx-focus-color: red;");
-		Password.setStyle("-fx-text-box-border: red; -fx-focus-color: red;"); */
+		Password.setStyle("-fx-text-box-border: red; -fx-focus-color: red;");
 	}
 	
 	/**
-	 * Reset default colors and clears.
+	 * Reset default colors and clears username field.
 	 * @param e Mouse event.
 	 */
 	public void resetUsername(MouseEvent e){
 		Username.setStyle("-fx-text-box-border: white; -fx-focus-color: #008ED6;");
+		userwrong.setStyle("-fx-opacity: 0;");
 	}
 	
 	/**
-	 * Reset default colors and clears.
+	 * Reset default colors and clears password field.
 	 * @param e Mouse event.
 	 */
 	public void resetPassword(MouseEvent e){
 		Password.setStyle("-fx-text-box-border: white; -fx-focus-color: #008ED6;");
+		Password.clear();
+		passwrong.setStyle("-fx-opacity: 0;");
 	}
 }
