97dfe4047754531bb560a1815005b20cc9507677
vinceix <vincentxie12@gmail.com>
2016-04-03 20:41:43 -0400

    Adding albums to view

--- src/controller/AlbumsController.java
+++ src/controller/AlbumsController.java
@@ -12,6 +12,9 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.geometry.VPos;
 import javafx.scene.Node;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
@@ -24,6 +27,7 @@ import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 import model.Album;
 import view.AddAlbumDialog;
@@ -36,18 +40,6 @@ public class AlbumsController extends Controller implements Initializable{
 	
 	@FXML
 	private ImageView imageView;
-	
-	@FXML
-	private ImageView album1;
-	
-	@FXML
-	private Text name1;
-	
-	@FXML
-	private ImageView album2;
-	
-	@FXML
-	private Text name2;
 	
 	@FXML
 	private GridPane grid;
@@ -82,16 +74,31 @@ public class AlbumsController extends Controller implements Initializable{
 		imageView.setImage(image);	
 		
 		File file1 = new File("src/assets/test.jpeg");
-		Image image1 = new Image(file1.toURI().toString());
-		album1.setImage(image1);		
-		
-		Image image2 = new Image(file1.toURI().toString());
-		album2.setImage(image2);
-		
-		album1.addEventFilter(MouseEvent.MOUSE_PRESSED, event -> { segue("/view/Album.fxml"); });
-		name1.addEventFilter(MouseEvent.MOUSE_PRESSED, event -> System.out.println("test"));
-		album2.addEventFilter(MouseEvent.MOUSE_PRESSED, event -> System.out.println("test"));
-		name2.addEventFilter(MouseEvent.MOUSE_PRESSED, event -> System.out.println("test"));
+		Image image1 = new Image(file1.toURI().toString());	
+		for(int i = 0; i < 3; i++){
+			for(int j = 0; j < 2; j++){
+		        ImageView test = new ImageView();
+		        test.setFitHeight(190);
+		        test.setFitWidth(320);
+		        test.setPreserveRatio(true);
+		        test.setPickOnBounds(true);
+		        test.setImage(image1);
+		        grid.add(test, j, i);
+		        GridPane.setHalignment(test, HPos.CENTER);
+		        GridPane.setValignment(test, VPos.CENTER);
+		        GridPane.setMargin(test, new Insets(0, 0, 10, 0));
+		        test.addEventFilter(MouseEvent.MOUSE_PRESSED, event -> { segue("/view/Album.fxml"); });
+		     
+		     	Text test1 = new Text();
+		     	test1.setText("Album name");
+		     	test1.setWrappingWidth(366);
+		     	grid.add(test1, j, i);
+		        GridPane.setHalignment(test1, HPos.CENTER);
+		        GridPane.setValignment(test1, VPos.BOTTOM);
+		        GridPane.setMargin(test1, new Insets(0, 0, 10, 0));
+		        test1.addEventFilter(MouseEvent.MOUSE_PRESSED, event -> { segue("/view/Album.fxml"); });
+			}
+		}
 	}
 	
 	/**
